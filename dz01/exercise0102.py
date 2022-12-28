ad6534bc98077680a6872d07b2e24fcbfd367705
mic <mic_st@mail.ru>
2022-12-28 22:53:03 +0400

    Homework01

---
+++ dz01/exercise0102.py
@@ -0,0 +1,11 @@
+# 2. Напишите программу для проверки ложности утверждения
+# (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
+
+print('W X Y Z')
+
+for w in range (2):
+    for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                if not ((w and z) or (not y) or ((not x) == (not w))):
+                    print(w, x, y, z)
